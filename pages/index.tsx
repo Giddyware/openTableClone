f2620cd496ba1d94f31171b4fe7c82697508f4e4
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-06-15 06:07:32 +0100

    skeletion: homepage done

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,54 +14,25 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gray-100 min-h-screen w-screen">
-        <main className="max-w-screen-2xl m-auto bg-white">
+      <main className="w-screen min-h-screen bg-gray-100">
+        <main className="m-auto bg-white max-w-screen-2xl">
           {/* NAVBAR */}
 
-          <nav className="bg-white p-2 flex justify-between">
-            <a href="" className="font-bold text-gray-700 text-2xl">
-              OpenTable
+          <nav className="flex justify-between p-2 bg-white">
+            <a href="" className="text-2xl font-bold text-gray-700">
+              {" "}
+              OpenTable{" "}
             </a>
             <div>
               <div className="flex gap-3">
-                <button className="bg-blue-400 text-white border p-1 px-4 rounded">
+                <button className="p-1 px-4 text-white bg-blue-400 border rounded">
                   Sign in
                 </button>
-                <button className=" border p-1 px-4 rounded">Sign up</button>
+                <button className="p-1 px-4 border rounded ">Sign up</button>
               </div>
             </div>
           </nav>
           {/* NAVBAR */}
-
-          <main>
-            {/* HEADER */}
-            <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-              <div className="text-center mt-10">
-                <h1 className="text-white text-5xl font-bold mb-2">
-                  Find your table for any occasion
-                </h1>
-                {/* SEARCH BAR */}
-                <div className="text-left text-lg py-3 m-auto flex justify-center">
-                  <input
-                    type="text"
-                    className="rounded  mr-3 w-74 p-2 w-[450px]"
-                    placeholder="State, city or town"
-                  />
-                  <button className="bg-red-600 px-9 py-2 text-white rounded">
-                    Let's go
-                  </button>
-                </div>
-                {/* SEARCH BAR */}
-              </div>
-            </div>
-            {/* HEADER */}
-
-            {/* CARD */}
-            <div className="py-3 px-36 flex mt-10 flex-wrap">{/* CARD */}</div>
-            {/* CARD */}
-            
-            {/* CARD */}
-          </main>
         </main>
       </main>
     </>
